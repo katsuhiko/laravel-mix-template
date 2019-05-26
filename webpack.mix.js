7d71f20d96b6cca62c4772ace04a4780f4f87679
Katsuhiko Nagashima <katsuhiko.nagashima@gmail.com>
2019-05-26 20:31:55 +0900

    feat: css modules を利用できるようにしました。

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,4 +12,22 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+    .sass('resources/sass/app.scss', 'public/css')
+    .webpackConfig({
+      module: {
+        rules: [
+          {
+            test: /\.css$/,
+            loaders: [
+              {
+                loader: 'css-loader',
+                options: {
+                  modules: true,
+                  localIdentName: '[local]_[hash:base64:8]',
+                },
+              },
+            ],
+          },
+        ],
+      },
+    });
