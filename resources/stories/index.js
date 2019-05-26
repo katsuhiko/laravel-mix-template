6a3755154d8b86e2f7b9b73413218f619cb052ea
Katsuhiko Nagashima <katsuhiko.nagashima@gmail.com>
2019-05-26 14:21:51 +0900

    chore: フォーマットを修正しました。

--- resources/stories/index.js
+++ resources/stories/index.js
@@ -1,26 +1,23 @@
 import { storiesOf } from '@storybook/vue'
 
 import ExampleComponent from './../js/components/ExampleComponent.vue'
-
-storiesOf('ExampleComponent', module)
+storiesOf('example-component', module)
   .add('デフォルト', () => ({
     components: { ExampleComponent },
-    template: `<ExampleComponent />`
+    template: `<example-component />`
   }))
 
-
 import BalloonEl from './../js/components/atoms/BalloonEl.vue'
-
-storiesOf('BalloonEl', module)
-.add('2文字ラベル', () => ({
-  components: { BalloonEl },
-  template: `<BalloonEl>次へ</BalloonEl>`
-}))
-.add('4文字ラベル', () => ({
-  components: { BalloonEl },
-  template: `<BalloonEl>削除する</BalloonEl>`
-}))
-.add('絶対座標配置', () => ({
-  components: { BalloonEl },
-  template: `<BalloonEl :style="{ position: 'absolute', top: '200px', left: '200px' }">左上から 200px に配置</BalloonEl>`
-}))
+storiesOf('balloon-el', module)
+  .add('2文字ラベル', () => ({
+    components: { BalloonEl },
+    template: `<balloon-el>次へ</balloon-el>`
+  }))
+  .add('4文字ラベル', () => ({
+    components: { BalloonEl },
+    template: `<balloon-el>削除する</balloon-el>`
+  }))
+  .add('絶対座標配置', () => ({
+    components: { BalloonEl },
+    template: `<balloon-el :style="{ position: 'absolute', top: '200px', left: '200px' }">左上から 200px に配置</balloon-el>`
+  }))
