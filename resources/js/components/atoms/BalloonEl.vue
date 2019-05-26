81f23bb5357c924f4ed727b5486099e5d7afa412
Katsuhiko Nagashima <katsuhiko.nagashima@gmail.com>
2019-05-26 14:13:48 +0900

    feat: atom のサンプルを追加しました。

---
+++ resources/js/components/atoms/BalloonEl.vue
@@ -0,0 +1,34 @@
+<template>
+  <span class="balloon"><slot></slot></span>
+</template>
+
+<style scoped>
+.balloon {
+  background-color: #1a1a1a;
+  border-radius: 2px;
+  color: white;
+  display: inline-block;
+  font-size: 0.8rem;
+  padding: 0.4rem 0.5rem;
+  position: relative;
+}
+
+.balloon::after {
+  border-color: #1a1a1a transparent transparent transparent;
+  border-style: solid;
+  border-width: 3px 3px 0 3px;
+  bottom: 0;
+  content: "";
+  display: block;
+  height: 0;
+  left: 50%;
+  position: absolute;
+  transform: translate(-50%, 100%);
+  width: 0;
+}
+</style>
+
+<script>
+export default {
+}
+</script>
--- resources/stories/index.js
+++ resources/stories/index.js
@@ -3,7 +3,24 @@ import { storiesOf } from '@storybook/vue'
 import ExampleComponent from './../js/components/ExampleComponent.vue'
 
 storiesOf('ExampleComponent', module)
-  .add('simple', () => ({
+  .add('デフォルト', () => ({
     components: { ExampleComponent },
-    template: `<ExampleComponent></ExampleComponent>`
+    template: `<ExampleComponent />`
   }))
+
+
+import BalloonEl from './../js/components/atoms/BalloonEl.vue'
+
+storiesOf('BalloonEl', module)
+.add('2文字ラベル', () => ({
+  components: { BalloonEl },
+  template: `<BalloonEl>次へ</BalloonEl>`
+}))
+.add('4文字ラベル', () => ({
+  components: { BalloonEl },
+  template: `<BalloonEl>削除する</BalloonEl>`
+}))
+.add('絶対座標配置', () => ({
+  components: { BalloonEl },
+  template: `<BalloonEl :style="{ position: 'absolute', top: '200px', left: '200px' }">左上から 200px に配置</BalloonEl>`
+}))
